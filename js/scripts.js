b4f426e047c5e870cad35a0ebd55f7e7a80bd91c
AVanderheyden81 <109109402+AVanderheyden81@users.noreply.github.com>
2022-09-11 09:36:42 -0600

    Create scripts.js
    Revisions made per linter

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+$(document).ready(function(){
+  $(".hamburger").click(function(){
+    $(this).toggleClass("is-active");
+  });
+});
+
+//apply a function for the onclick attribute which has been assigned a value of "menuToggle". Then assign a variable to the hamburger menu object telling it to search the document using the method which returns the value by looking for a matching id in the HTMl in this case "myNavtoggle" and assigns that value to X.
+
+// X is now equal myNavtoggle
+
+//to ensure the menu items appear/dissapear on click X (myNavtoggle) now needs to be made responsive by applying the property (.class.Name)  to X telling it to be responsive by adding the ' responsive' class to the current/clicked (+=) hamburger menu.
+
+// X (myNavtoggle) is now also reponsive
+
+////////////////////////////////////////////////////////
+
+// call for Owl initializer function for carousel
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+
+// Auto width style caroseul function
+$('.owl-carousel').owlCarousel({
+    margin:100,
+    loop:false,
+    autoWidth:false,
+    items:1
+})
