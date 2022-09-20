e9cda3a058c28ce7e21ff4fd5541b8d415586384
AVanderheyden81 <109109402+AVanderheyden81@users.noreply.github.com>
2022-09-19 19:04:21 -0600

    Update scripts.js
    side by side rules added

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,17 @@ $('.owl-carousel').owlCarousel({
     autoWidth:false,
     items:1
 })
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
